9b2ea07507bcc19e3260419a5a7f464d50a82613
Peter Erdos <erdospepe9@gmail.com>
2024-02-27 21:24:52 +0100

    Fixed portal spawn
    Now it selects the tiles that are floor and everytime a debug button is pressed it spawns a portal in a random location

--- main.gd
+++ main.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
 @onready var tilemap = $tilemap
+@onready var portal = preload("res://portal.tscn")
+
+var custom_data_tile_list = []
 
 func _ready():
 	#print("Tilemap layer count:", tilemap.get_used_cells(0))
 	var tilemap_list = tilemap.get_used_cells(0)
 	for element in tilemap_list:
 		var tiledata = tilemap.get_cell_tile_data(0, element)
-		print(tiledata.get_custom_data("d"))
+		if tiledata.get_custom_data("d"):
+			custom_data_tile_list.append(element)	
+		#print(tilemap.map_to_local(element))
+
+func _process(delta):
+	if Input.is_action_just_pressed("spawn portal"):
+		var newPortal = portal.instantiate()
+		if newPortal != null:
+			var rand_index:int = randi() % custom_data_tile_list.size()
+			print(rand_index)
+			print(custom_data_tile_list[rand_index])
+			newPortal.position = tilemap.map_to_local(custom_data_tile_list[rand_index])
+			add_child(newPortal)
+
---
+++ portal.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+#@onready var slime_enemy = preload("res://enemy.tscn")
+#@onready var wind_enemy = preload("res://wind_enemy.tscn")
+@onready var label = $Label
+var timeout_count = 0
+
+func _ready():
+	label.set_tooltip_text(str(self.position.x) +" " + str(self.position.y))
+
+func _process(delta):
+	if timeout_count == 5:
+		queue_free()
+
+#func _on_timer_timeout():
+	#timeout_count += 1
+	#var random_value = randf()
+	#var enemy_instance = null
+	
+	#if random_value < 0.25:
+	#	enemy_instance = wind_enemy.instantiate()
+	#else:
+	#	enemy_instance = slime_enemy.instantiate()
+
+	#if enemy_instance:
+	#	enemy_instance.global_position = self.global_position
+	#	get_parent().add_child(enemy_instance)
